d191113d07ca1c01b69c793efffa3893aaa018c0
Luigi Rauso <darjkzer@hotmail.it>
2023-12-15 12:47:02 +0100

    07 - exercise completed

--- books_table.sql
+++ books_table.sql
@@ -42,13 +42,13 @@ WHERE
     title = 'War and Peace';
 
 -- EX5: GRANT
-GRANT SELECT, UPDATE ON Books TO 'Martin'@'localhost'
+GRANT SELECT, UPDATE ON Books TO 'Martin'@'localhost';
 
 -- EX6: REVOKE
-REVOKE UPDATE ON Books FROM 'Martin'@'localhost'
+REVOKE UPDATE ON Books FROM 'Martin'@'localhost';
 
 -- EX7: COMMIT
-BEGIN TRANSACTION
-DELETE FROM Books WHERE book_id = 101
-SAVEPOINT SP1
-COMMIT
+BEGIN TRANSACTION;
+DELETE FROM Books WHERE book_id = 101;
+SAVEPOINT SP1;
+COMMIT;
