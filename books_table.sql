0d235be1b6744a128b823515ddc82459bfb7b6c0
Luigi Rauso <darjkzer@hotmail.it>
2023-12-15 09:49:05 +0100

    03 - exercise completed

--- books_table.sql
+++ books_table.sql
@@ -18,3 +18,16 @@ ADD
     publisher VARCHAR(255),
 ADD
     number_of_pages INT;
+
+-- EX3: INSERT INTO
+INSERT INTO Books (book_id, title, author, genre, published_year, isbn, price, rating, stock_count, publisher, number_of_pages)
+VALUES (1, 'A Journey to the Center', 'Jules Verne', 'Adventure', 1864, '1234567890123', 12.99, 4.5, 10, 'Verne Publishing', 350);
+
+INSERT INTO Books (book_id, title, author, genre, published_year, isbn, price, rating, stock_count, publisher, number_of_pages)
+VALUES (2, 'War and Peace', 'Leo Tolstoy', 'Historical', 1869, '1234567890124', 14.99, 4.8, 5, 'Tolstoy Prints', 1200);
+
+INSERT INTO Books (book_id, title, author, genre, published_year, isbn, price, rating, stock_count, publisher, number_of_pages)
+VALUES (3, 'Whispers of the Wind', 'Amelia Blackburn', 'Romance', 1982, '1234567890125', 9.99, 4.2, 20, 'Blackburn House', 275);
+
+INSERT INTO Books (book_id, title, author, genre, published_year, isbn, price, rating, stock_count, publisher, number_of_pages)
+VALUES (4, 'The Galactic Odyssey', 'Orion Starfield', 'Science Fiction', 2005, '1234567890126', 19.99, 4.9, 15, 'Nebula Press', 450);
